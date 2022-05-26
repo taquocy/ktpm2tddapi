7ef981b4f451d2aea28e99fd1b41ad0a6ce017f8
taquocy <taquocitali@gmail.com>
2022-05-26 13:23:45 +0700

    init project

---
+++ flask-tdd-docker/src/config.py
@@ -0,0 +1,22 @@
+# src/config.py
+
+import os  # new
+
+
+class BaseConfig:
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'my_precious'  # new
+
+
+class DevelopmentConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')  # new
+
+
+class ProductionConfig(BaseConfig):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')  # new
